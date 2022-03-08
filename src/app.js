1af7c118a64b4b8d03121c83087b243ab9b25b86
ys <1455244@qq.com>
2022-03-08 15:02:17 +0800

    feat 增加session配置

--- src/app.js
+++ src/app.js
@@ -9,12 +9,16 @@ const logger = require('koa-logger')
 const index = require('./routes/index')
 const users = require('./routes/users')
 
+const session = require('koa-generic-session')
+const redisStore = require('koa-redis')
+// 不是｛REDIS_CONF｝
+const REDIS_CONF = require('./conf/db')
 // error handler
 onerror(app)
 
 // middlewares
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+  enableTypes: ['json', 'form', 'text']
 }))
 app.use(json())
 app.use(logger())
@@ -32,6 +36,20 @@ app.use(async (ctx, next) => {
   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
 })
 
+// 对userid加密
+app.keys = ['ieI8EE3J_&6*']
+app.use(session({
+  key: 'weibo.sid', //cookie的名字
+  prefix: 'weibo:sess:', //redis的key的名字
+  cookie: {
+    path: '/',
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000
+  },
+  store: redisStore({
+    all: `${REDIS_CONF.host}:${REDIS_CONF.port}`
+  })
+}))
 // routes
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
--- src/routes/index.js
+++ src/routes/index.js
@@ -10,14 +10,20 @@ router.get('/:username/:pageIndex', async (ctx, next) => {
   const { username, pageIndex } = ctx.params
   ctx.body = {
     name: username,
-    index: pageIndex
+    index: pageIndex,
   }
 })
 
 
 router.get('/json', async (ctx, next) => {
+  const session = ctx.session
+  if (session.viewNumber == null) {
+    session.viewNumber = 0
+  }
+  session.viewNumber++
   ctx.body = {
-    title: 'koa2 json'
+    title: 'koa2 json',
+    a: session.viewNumber
   }
 })
 
