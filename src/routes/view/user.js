538441875f39719135e9d0d0f823becbbadea94d
ys <1455244@qq.com>
2022-03-10 15:58:45 +0800

    feat: 增加setting页面模版

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -1,3 +1,5 @@
+const { loginRedirect } = require('../../middlewares/loginCheck')
+
 /**
  * @description view user router
  * @author 杨硕
@@ -27,6 +29,10 @@ router.get('/login', async (ctx, next) => {
 
 router.get('/register',async (ctx, next) => {
     await ctx.render('register')
+})
+
+router.get('/setting',loginRedirect, async (ctx, next) => {
+    await ctx.render('setting',ctx.session.userInfo)
 })
 
 module.exports = router
