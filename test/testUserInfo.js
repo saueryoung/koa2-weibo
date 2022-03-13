de3523fcca05700f1bd11bc2bf08e7d3c573a9a3
ys <1455244@qq.com>
2022-03-13 17:02:24 +0800

    feat: 关注和取关的单元测试

--- test/testUserInfo.js
+++ test/testUserInfo.js
@@ -3,7 +3,12 @@
  * @author 杨硕
  */
 
-module.exports = {
-    Z_COOKIE: 'weibo.sid=Rn160YuO_7HuqU-0oZhd-L8HPggI_bx5; weibo.sid.sig=ATabgPALcMj9CvY7l-uA_Ft2yt4',
+ module.exports = {
+    Z_ID: 4,
     Z_USER_NAME: 'zhangsan',
+    Z_COOKIE: 'weibo.sid=Rn160YuO_7HuqU-0oZhd-L8HPggI_bx5; weibo.sid.sig=ATabgPALcMj9CvY7l-uA_Ft2yt4',
+
+    L_ID: 5,
+    L_USER_NAME: 'lisi',
+    L_COOKIE: 'weibo.sid=Rn160YuO_7HuqU-0oZhd-L8HPggI_bx5; weibo.sid.sig=ATabgPALcMj9CvY7l-uA_Ft2yt4'
 }
