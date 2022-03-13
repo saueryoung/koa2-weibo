3957cc79074b9d71bac165b8337ebeb95afd3eef
ys <1455244@qq.com>
2022-03-13 21:39:35 +0800

    feat: 首页加载博客 单元测试

--- test/blog/home.test.js
+++ test/blog/home.test.js
@@ -29,3 +29,16 @@ test('创建一条微博，应该成功', async () => {
     // 记录微博 id
     BLOG_ID = res.body.data.id
 })
+
+test('测试首页，第一页加载，应该成功', async () => {
+    const res = await server
+        .get(`/api/blog/loadMore/0`)
+        .set('cookie', Z_COOKIE)
+    expect(res.body.errno).toBe(0)
+    const data = res.body.data
+    expect(data).toHaveProperty('isEmpty')
+    expect(data).toHaveProperty('blogList')
+    expect(data).toHaveProperty('count')
+    expect(data).toHaveProperty('pageSize')
+    expect(data).toHaveProperty('pageIndex')
+})
